e1ae20232086e3e1da9319efe034a91370c5464a
kouacc <maxrur57@gmail.com>
2024-04-12 08:13:13 +0200

    import ref et declaration menu

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-import { onErrorCaptured } from 'vue'
+import { onErrorCaptured, ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router/auto'
 
 onErrorCaptured((err, instance, info) => {
   console.error('erreur : ', err, '\ninfo : ', info, '\ncomposant : ', instance)
   return true
 })
+
+const menuIsOpen = ref(false)
 </script>
 
 <template>
