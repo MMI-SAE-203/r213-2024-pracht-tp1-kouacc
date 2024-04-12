4b134ab1ef2cce3fc547f39bc8cabdf271de778b
kouacc <maxrur57@gmail.com>
2024-04-12 09:58:21 +0200

    debut carrousel

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,6 +3,8 @@ import { ref } from 'vue'
 const sectionOpen = ref(1)
 const sectionOpenData = ref()
 
+const carrouselActif = ref(0)
+
 const sectionsData = [
   {
     label: 'bouton 1',
@@ -57,14 +59,122 @@ const sectionsData = [
       sint doloremque eos officiis?
     </p>
   </section>
-  <section v-for="({ label, texte }, key) of sectionsData" :v-key="key">
-    <button class="text-xl" @pointerdown="sectionOpenData = sectionOpenData === key ? null : key">
+  <section v-for="({ label, texte }, key) of sectionsData" :key="key">
+    <button class="text-xl" @pointerdown="sectionOpenData = sectionOpenData === (key+1) ? null : (key+1)">
       {{ label }} (data json)
     </button>
-    <section v-show="sectionOpenData === key">
+    <section v-show="sectionOpenData === (key+1)">
       <pre class="font-mono">key : {{ key }}</pre>
       <pre class="font-mono">label : {{ label }}</pre>
       <pre class="font-mono">texte : {{ texte }}</pre>
     </section>
   </section>
+  <div class="relative">
+    <button
+      class="grid place-content-center absolute top-2/4 left-0 bg-black/50 rounded-full text-white p-8 w-8 cursor-pointer"
+      aria-label="slide précedent"
+      @pointerdown="carrouselActif = carrouselActif === 0 ? 0 : carrouselActif - 1"
+    >
+      <
+    </button>
+    <button
+      class="grid place-content-center absolute top-2/4 right-0 bg-black/50 rounded-full text-white p-8 w-8 cursor-pointer"
+      aria-label="slide suivant"
+      @pointerdown="carrouselActif = carrouselActif === 4 ? 4 : carrouselActif + 1"
+    >
+      >
+    </button>
+    <div
+      class="flex w-full overflow-x-scroll overflow-hidden rounded-lg snap-mandatory snap-x scroll-smooth"
+      x-ref="parent"
+      @scroll=""    >
+      <div id="ecran-1" class="flex-none w-full text-center snap-start">
+        <figure class="m-0">
+          <img
+            class="h-auto w-max"
+            src="https://picsum.photos/seed/1/500/300"
+            alt="image ecran 1"
+          />
+          <figcaption>Écran 1</figcaption>
+        </figure>
+      </div>
+      <div id="ecran-2" class="flex-none w-full text-center snap-start">
+        <figure class="m-0">
+          <img
+            class="h-auto w-max"
+            src="https://picsum.photos/seed/2/500/300"
+            alt="image ecran 2"
+          />
+          <figcaption>Écran 2</figcaption>
+        </figure>
+      </div>
+      <div id="ecran-3" class="flex-none w-full text-center snap-start">
+        <figure class="m-0">
+          <img
+            class="h-auto w-max"
+            src="https://picsum.photos/seed/3/500/300"
+            alt="image ecran 3"
+          />
+          <figcaption>Écran 3</figcaption>
+        </figure>
+      </div>
+      <div id="ecran-4" class="flex-none w-full text-center snap-start">
+        <figure class="m-0">
+          <img
+            class="h-auto w-max"
+            src="https://picsum.photos/seed/4/500/300"
+            alt="image ecran 4"
+          />
+          <figcaption>Écran 4</figcaption>
+        </figure>
+      </div>
+      <div id="ecran-5" class="flex-none w-full text-center snap-start">
+        <figure class="m-0">
+          <img
+            class="h-auto w-max"
+            src="https://picsum.photos/seed/5/500/300"
+            alt="image ecran 5"
+          />
+          <figcaption>Écran 5</figcaption>
+        </figure>
+      </div>
+    </div>
+    <ul class="flex justify-center gap-4">
+      <li>
+        <a
+          href="#ecran-1"
+          class="bg-gray-300 rounded-md p-2 flex mt-3 hover:bg-yellow-500 transition-all"
+          >Écran 1</a
+        >
+      </li>
+      <li>
+        <a
+          href="#ecran-2"
+          class="bg-gray-300 rounded-md p-2 flex mt-3 hover:bg-yellow-500 transition-all"
+          >Écran 2</a
+        >
+      </li>
+      <li>
+        <a
+          href="#ecran-3"
+          class="bg-gray-300 rounded-md p-2 flex mt-3 hover:bg-yellow-500 transition-all"
+          >Écran 3</a
+        >
+      </li>
+      <li>
+        <a
+          href="#ecran-4"
+          class="bg-gray-300 rounded-md p-2 flex mt-3 hover:bg-yellow-500 transition-all"
+          >Écran 4</a
+        >
+      </li>
+      <li>
+        <a
+          href="#ecran-5"
+          class="bg-gray-300 rounded-md p-2 flex mt-3 hover:bg-yellow-500 transition-all"
+          >Écran 5</a
+        >
+      </li>
+    </ul>
+  </div>
 </template>
