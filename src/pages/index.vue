38d2a333986ec8521bd4ba6f0cb7d2b3cda8f337
kouacc <maxrur57@gmail.com>
2024-04-12 08:18:09 +0200

    ajout boutons et lorem index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,4 +4,16 @@
 
 <template>
   <h1 class="text-2xl">Bonjour monde !</h1>
+  <section>
+    <button class="text-xl">bouton 1</button>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui, explicabo placeat. Nisi officia dolorum dolor quas aliquid. Veniam quidem odio quisquam tempora dolorum illum vel quas? Molestias ipsa facere et?</p>
+  </section>
+  <section>
+    <button class="text-xl">bouton 2</button>
+    <p>Voluptate adipisci vitae distinctio autem omnis, deserunt rem optio dolor dicta fugiat sit non veritatis eum molestias odit, nulla magnam exercitationem? Exercitationem ratione quia nihil. Molestiae provident rerum esse ratione?</p>
+  </section>
+  <section>
+    <button class="text-xl">bouton 3</button>
+    <p>Saepe assumenda praesentium suscipit incidunt vel veritatis ad tempore, ut sapiente itaque rerum sit voluptatem eveniet aliquid delectus quae velit a error quia. Quaerat unde voluptatum sint doloremque eos officiis?</p>
+  </section>
 </template>
